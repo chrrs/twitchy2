7429676d221d8db6614d79cbfa26b751f477c6a3
Chris <chirssel@gmail.com>
2021-11-16 15:43:32 +0100

    Display some Twitch chat

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,24 @@
-<script lang="typescript">
-    let world = 'world!';
+<script lang="ts">
+    import VirtualList from '@sveltejs/svelte-virtual-list';
+    import { ChatClient } from '@twurple/chat';
+
+    let scrollToIndex: (index: number, cfg?: ScrollToOptions) => void;
+    let messages = [];
+
+    (async () => {
+        const client = new ChatClient({ channels: ['Gaules'] });
+        await client.connect();
+
+        client.onMessage(async (_channel, user, message) => {
+            messages = [...messages, { user, message }];
+            scrollToIndex(messages.length - 1, { behavior: 'auto' });
+            console.log(user, message);
+        });
+    })();
 </script>
 
-Hello {world}
+<div class="h-screen max-w-5xl mx-auto">
+    <VirtualList items={messages} bind:scrollToIndex let:item>
+        <p><b>{item.user}: </b>{item.message}</p>
+    </VirtualList>
+</div>
