57487643a9034f3eafb162d17c49801ba076b4e4
Chris <chirssel@gmail.com>
2021-11-18 17:04:56 +0100

    Separate a ChatMessage component

--- src/App.svelte
+++ src/App.svelte
@@ -1,8 +1,8 @@
 <script lang="ts">
     import VirtualList from './components/VirtualList.svelte';
+    import ChatMessage from './components/ChatMessage.svelte';
     import { ChatClient } from '@twurple/chat';
     import { Message, parseMessage } from './lib/message';
-    import tooltip from './lib/tooltip';
     import { globalBadgeProvider } from './lib/badge';
     import { bttvGlobalEmoteProvider } from './lib/bttv';
     import { ffzGlobalEmoteProvider } from './lib/ffz';
@@ -46,43 +46,7 @@
         scrollToBottom={!pauseAutoScroll}
         let:item
     >
-        <p class="min-h-6 mb-1">
-            <span>
-                {#each item.author.badges as badge}
-                    <img
-                        class:bg-red-500={!badge.url}
-                        class:text-transparent={!badge.url}
-                        class="inline-block w-5 h-5 not-last:mr-1 object-contain align-text-bottom"
-                        src={badge.url}
-                        alt={badge.name}
-                        use:tooltip={{
-                            // FIXME: Potential XSS
-                            content: `<b>${badge.name}</b>`,
-                            allowHTML: true,
-                        }}
-                    />
-                {/each}
-            </span>
-            <b class="font-semibold" style={`color: ${item.author.color}`}>
-                {item.author.name}:
-            </b>
-            {#each item.message as part}
-                {#if part.type === 'text'}
-                    {part.text}
-                {:else if part.type === 'emote'}
-                    <img
-                        class="inline-block h-6 object-contain align-bottom"
-                        src={part.url}
-                        alt={part.name}
-                        use:tooltip={{
-                            // FIXME: Potential XSS
-                            content: `<b>${part.name}</b><br/>${part.description}`,
-                            allowHTML: true,
-                        }}
-                    />
-                {/if}
-            {/each}
-        </p>
+        <ChatMessage message={item} />
     </VirtualList>
 </div>
 
