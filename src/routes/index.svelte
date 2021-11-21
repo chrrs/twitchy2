8839ccf13ec53fc04ebbc24702a87a5c98915343
Chris <chirssel@gmail.com>
2021-11-21 20:06:39 +0100

    Redirect to the chat page when logged in

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+	import { goto } from '$app/navigation';
+
 	import { accounts } from '$store/accounts';
 	import { onMount } from 'svelte';
 
@@ -6,18 +8,27 @@
 
 	onMount(async () => {
 		await accounts.fetch();
-		loading = false;
+
+		if ($accounts.length !== 0) {
+			goto('/chat');
+		} else {
+			loading = false;
+		}
 	});
+
+	async function tryLogin() {
+		await accounts.tryLogin();
+
+		if ($accounts.length !== 0) {
+			goto('/chat');
+		}
+	}
 </script>
 
 <div class="h-full flex justify-center items-center">
 	{#if loading}
 		<span>Loading...</span>
-	{:else if $accounts.length === 0}
-		<button on:click={() => accounts.tryLogin()}>Log in</button>
 	{:else}
-		<span>
-			{JSON.stringify($accounts)}
-		</span>
+		<button on:click={tryLogin}>Log in</button>
 	{/if}
 </div>
