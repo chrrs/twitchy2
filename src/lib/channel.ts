cf50f15fbf7d5269a01664a0cc75a074d57dfa78
Chris <chirssel@gmail.com>
2021-11-22 19:30:07 +0100

    Add event support to channels

--- src/lib/channel.ts
+++ src/lib/channel.ts
@@ -1,13 +1,35 @@
+import type { Emitter, Handler } from 'mitt';
+import mitt from 'mitt';
 import { getApiClient } from './api';
+import type { Message } from './message';
 
 const channels: { [key: string]: Channel } = {};
 
+export type ChannelEvents = {
+	message: Message;
+};
+
 export class Channel {
+	mitt: Emitter<ChannelEvents>;
+
 	name: string;
 	id: string;
 
 	isLive: boolean;
 	refs: number;
+
+	constructor() {
+		this.refs = 1;
+		this.mitt = mitt();
+	}
+
+	on<Key extends keyof ChannelEvents>(type: Key, handler: Handler<ChannelEvents[Key]>): void {
+		this.mitt.on(type, handler);
+	}
+
+	off<Key extends keyof ChannelEvents>(type: Key, handler?: Handler<ChannelEvents[Key]>): void {
+		this.mitt.off(type, handler);
+	}
 
 	drop() {
 		this.refs--;
@@ -32,7 +54,6 @@ export async function fetchChannel(name: string): Promise<Channel> {
 	channel.id = info.id;
 	channel.name = info.displayName;
 	channel.isLive = (await info.getStream()) !== null;
-	channel.refs = 1;
 
 	return channel;
 }
